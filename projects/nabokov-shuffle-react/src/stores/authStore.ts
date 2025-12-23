cb1144fc0bb09635f0bf7f4aa7c3db0a5f84cf22
AbayReushenov <abay.reushenov@gmail.com>
2025-12-23 20:09:43 +0300

    Test 2 5

--- projects/nabokov-shuffle-react/src/stores/authStore.ts
+++ projects/nabokov-shuffle-react/src/stores/authStore.ts
@@ -2,7 +2,7 @@ import { create } from 'zustand';
 import { supabase } from '../config/supabase';
 import type { User, AuthState } from '../types';
 
-// test 2
+// test 2 55
 
 interface AuthStoreState extends AuthState {
   signInWithMagicLink: (email: string) => Promise<void>;
